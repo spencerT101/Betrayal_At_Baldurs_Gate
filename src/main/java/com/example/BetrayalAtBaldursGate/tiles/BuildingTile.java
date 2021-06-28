630130be2fda122a44d2d91b9836fb4d033340a6
Spencer <Spencer.Taber@yahoo.com>
2021-06-28 20:54:27 +0100

    Building Tile class created

---
+++ src/main/java/com/example/BetrayalAtBaldursGate/tiles/BuildingTile.java
@@ -0,0 +1,8 @@
+package com.example.BetrayalAtBaldursGate.tiles;
+
+public class BuildingTile extends Tile{
+
+    public BuildingTile(Boolean northDoor, Boolean eastDoor, Boolean southDoor, Boolean westDoor) {
+        super(northDoor, eastDoor, southDoor, westDoor);
+    }
+}
