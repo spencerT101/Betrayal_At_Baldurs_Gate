15ba98bd803060cbf9159443b6959f5380dd208b
Spencer <Spencer.Taber@yahoo.com>
2021-06-28 20:48:43 +0100

    Hero child class created

---
+++ src/main/java/com/example/BetrayalAtBaldursGate/characters/Hero.java
@@ -0,0 +1,8 @@
+package com.example.BetrayalAtBaldursGate.characters;
+
+public class Hero extends Character {
+
+    public Hero(String name, int might, int speed, int sanity, int knowledge, String species) {
+        super(name, might, speed, sanity, knowledge, species);
+    }
+}
