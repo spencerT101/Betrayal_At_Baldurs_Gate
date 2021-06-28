7ec89fc0b090175904fca1eb158d9511f1c6ae73
Spencer <Spencer.Taber@yahoo.com>
2021-06-28 21:08:44 +0100

    Omen Card class with hauntroll method created

---
+++ src/main/java/com/example/BetrayalAtBaldursGate/cards/OmenCard.java
@@ -0,0 +1,13 @@
+package com.example.BetrayalAtBaldursGate.cards;
+
+public class OmenCard extends Card{
+
+    public OmenCard(String flavourText) {
+        super(flavourText);
+    }
+
+
+    public void hauntRoll(){
+
+    }
+}
