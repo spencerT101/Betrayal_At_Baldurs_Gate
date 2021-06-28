4732393fd9180efa6eeb5bbbeacfc3c0eabaec71
Spencer <Spencer.Taber@yahoo.com>
2021-06-28 20:53:34 +0100

    Street tiles class created

---
+++ src/main/java/com/example/BetrayalAtBaldursGate/tiles/StreetTile.java
@@ -0,0 +1,8 @@
+package com.example.BetrayalAtBaldursGate.tiles;
+
+public class StreetTile extends Tile{
+
+    public StreetTile(Boolean northDoor, Boolean eastDoor, Boolean southDoor, Boolean westDoor) {
+        super(northDoor, eastDoor, southDoor, westDoor);
+    }
+}
