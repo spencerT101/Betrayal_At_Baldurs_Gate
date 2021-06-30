d40526e78eb16a92c4fe87bc6cb255a05182e392
Spencer <Spencer.Taber@yahoo.com>
2021-06-30 15:30:36 +0100

    added whispering skull to data loader.

--- src/main/java/com/example/BetrayalAtBaldursGate/components/DataLoader.java
+++ src/main/java/com/example/BetrayalAtBaldursGate/components/DataLoader.java
@@ -50,8 +50,6 @@ public class DataLoader implements ApplicationRunner {
         Hero Gretchen = new Hero("Gretchen Titchwillow", 4, 4, 4,3, "Halfling", 8,8,6,8,"image", "Druid", 112);
         heroRepository.save(Gretchen);
 
-        Monster monster = new Monster("Test", 4, 4, 4, 4, "Test", "test");
-        monsterRepository.save(monster);
 
         BuildingTile tileWonder = new BuildingTile("Hall of Wonders", Door.FALSE, Door.FALSE, Door.YELLOW, Door.RED, Icon.EVENT);
         buildingTileRepository.save(tileWonder);
@@ -59,13 +57,8 @@ public class DataLoader implements ApplicationRunner {
         StreetTile ambushAlley = new StreetTile("Ambush Alley", Door.YELLOW, Door.FALSE, Door.YELLOW, Door.FALSE,Icon.EVENT);
         streetTileRepository.save(ambushAlley);
 
-        OmenCard omenCard = new OmenCard("Care Bear Vision","Bartholomew","Care bears rule!");
-        omenCardRepository.save(omenCard);
-
-        EventCard eventCard = new EventCard("Death Vision", "Barry", "text example");
-        eventCardRepository.save(eventCard);
-
-        OmenCard berserkerAxe = ()
+        OmenCard whisperingSkull = new OmenCard ("A small skull carved from jade. You hear vague, distant whispers as you look into it's empty eyes", "Whispering Skull","Once during your turn after the haunt has begun, you mat move 1 small monster or NPC 1 space.");
+        omenCardRepository.save(whisperingSkull);
 
     }
 }
