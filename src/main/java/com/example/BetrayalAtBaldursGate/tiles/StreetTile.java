e19a3e202b021905113444753ef60e3789cf7c0e
Spencer <Spencer.Taber@yahoo.com>
2021-06-30 13:53:44 +0100

    Routes working and displaying JSON objects in the browser.

--- src/main/java/com/example/BetrayalAtBaldursGate/tiles/StreetTile.java
+++ src/main/java/com/example/BetrayalAtBaldursGate/tiles/StreetTile.java
@@ -8,4 +8,8 @@ public class StreetTile extends Tile{
     public StreetTile(String name, Door northDoor, Door eastDoor, Door southDoor, Door westDoor, Icon icon) {
         super(name, northDoor, eastDoor, southDoor, westDoor, icon);
     }
+
+    public StreetTile(){
+
+    }
 }
