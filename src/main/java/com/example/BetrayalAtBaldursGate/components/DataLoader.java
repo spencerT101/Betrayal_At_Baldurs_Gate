2eff64af9e97025e3df782da15eeea99223faefd
klamb95 <klamb1995@gmail.com>
2021-06-30 15:34:00 +0100

    more cards

--- src/main/java/com/example/BetrayalAtBaldursGate/components/DataLoader.java
+++ src/main/java/com/example/BetrayalAtBaldursGate/components/DataLoader.java
@@ -54,11 +54,18 @@ public class DataLoader implements ApplicationRunner {
         EventCard aHagsBlessing = new EventCard("A stooped figure clad in a tattered robe stumbles toward you. She studies you with milky cataract-filled eyes before pointing at you and speaking", "A Hag's Blessing", "Roll 2 dice: 4: She whispers forbidden love. Gain 1 knowledge, 3: She offers a spell of protection gain 1 Sanity. 3: She mutters incoherently before shuffling on. 1: She promises death and ruin, lose 1 sanity. 0: She curses you. The next time you would roll for anything except for the haunt treat the results as 0");
         eventCardRepository.save(aHagsBlessing);
 
-        EventCard aHagsBlessing = new EventCard("A stooped figure clad in a tattered robe stumbles toward you. She studies you with milky cataract-filled eyes before pointing at you and speaking", "A Hag's Blessing", "Roll 2 dice: 4: She whispers forbidden love. Gain 1 knowledge, 3: She offers a spell of protection gain 1 Sanity. 3: She mutters incoherently before shuffling on. 1: She promises death and ruin, lose 1 sanity. 0: She curses you. The next time you would roll for anything except for the haunt treat the results as 0");
-        eventCardRepository.save(aHagsBlessing);
+        EventCard arcaneGate = new EventCard("A portal springs to life here, offering a shimmering passage to another location", "Arcane Gate", "Place 1 square Arcane Gate token on this tile, and the second square Arcane Gate token on any other tile on the board. An adventure, monster or NPC can move between those tiles with 1 space of movement");
+        eventCardRepository.save(arcaneGate);
 
+        EventCard bhallsCall = new EventCard("You have a vision of a grotesque skull, or blood pools around your feet and your ears", "Bhaal's Call", "You must attempt a sanity roll: 4+ You find the vision oddly compelling, Gain 1 might. 2-3: Lose 1 sanity. 0-1: Your mind is attacked. If your mind is attacked, the player on your right rolls a Sanity 3 attacked against you. You dont roll defence. Instead, if the result is higher than your Sanity, you take mental damage equal to the difference between the two");
+        eventCardRepository.save(bhallsCall);
 
-        EventCard willOWisp = new EventCard("A flickering light appears in the gloom ahead. It bobs and weaves in the air, and you feel an overwhelming compulsion to follow it.", "Will-O'-Wisp", "You ");
+        EventCard vampiricMist = new EventCard("","","")
+
+        EventCard wereratBite = new EventCard("You look down and see a fat, horrid-looking rat scamper away as you notice the pain in your foot. You suddenly don't feel so well.", "Wererat Bite", "You have been infected with lycanthropy. Keep this card in front of you. Lose 1 Knowledge and 1 Sanity. Gain 1 Might and Speed. If you inflict physical damage to an adventurer on your tile with an attack, you may choose to also infect them with lycanthropy. If you do, they lose 1 Knowledge and 1 Sanity, and gain 1 Might and 1 Speed. They can then infect other adventurers using the same rules. Each adventurer can be infected with lycanthropy only once per game.");
+        eventCardRepository.save(wereratBite);
+
+        EventCard willOWisp = new EventCard("A flickering light appears in the gloom ahead. It bobs and weaves in the air, and you feel an overwhelming compulsion to follow it.", "Will-O'-Wisp", "You must attempt a Knowledge roll: 4+ You resist the urge to follow the light. Gain 1 Knowledge. 2-3 Remove your adventurer figure from the board. At the start of your next turn, place your figure on any revealed omen tile, or the Elfsong Tavern tile if there are no omen tiles. Take 1 die of physical damage. 0-1 You follow the will-o'wisp in circles. Place your adventurer figure on the Elfsong Tavern tile.");
         eventCardRepository.save(willOWisp);
 
         EventCard yellowMold = new EventCard("Clumps of sickly yellow mold grow here. Spores erupt from the mold, and a coughing fit overcomes you.", "Yellow Mold", "Place the square Yellow Mold token on this tile and this card near it. Any time an adventurer enters the tile, including when you draw this card, that adventurer must attempt a Might roll: 4+ Puny spores can't hurt you. 1-3 This cough is going to last a while. Lose 1 Might. 0 You double over in pain. Take 1 die of physical damage.");
