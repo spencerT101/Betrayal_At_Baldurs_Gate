1c9986b715b8f947ea58c854217324cc02907e2e
Lina <31240383+linabiel@users.noreply.github.com>
2021-06-30 16:32:10 +0100

    top half omen cards  created in DataLoader.

--- src/main/java/com/example/BetrayalAtBaldursGate/components/DataLoader.java
+++ src/main/java/com/example/BetrayalAtBaldursGate/components/DataLoader.java
@@ -65,7 +65,89 @@ public class DataLoader implements ApplicationRunner {
         EventCard eventCard = new EventCard("Death Vision", "Barry", "text example");
         eventCardRepository.save(eventCard);
 
-        OmenCard berserkerAxe = ()
+
+
+
+        OmenCard berserkerAxe = new OmenCard(
+                "This axe was made to kill. It just needs you to help it fullfill its destiny",
+                "Berserker Axe",
+                "Your roll 2 additional dice (maximum of 8 dice) when making a Might attack with this weapon."
+                        + "If you are on a tile with one or more opponents, you cannot leave that tile unless you " +
+                        "first attack an opponent with this weapon." +
+                        "You can't use another weapon while you're using this one." +
+                        "This omen can't be dropped or traded, but it can be stolen."
+                );
+
+        OmenCard bookOfSpells = new OmenCard(
+                "This dusty tome is filled with secrets - some useful, others dangerous",
+                "Book of Spells",
+                "If you " +
+                "attack an opponent that has a Sanity trait, you may attack with Sanity instead of Might (your " +
+                "opponent defends with Sanity, amd damage is mental instead of physical)." +
+                "If you attack an opponent in this way and you take damage, take 1 additional point of damage." +
+                "You can't use another weapon while you're using this one."
+        );
+
+        OmenCard cursedArmor = new OmenCard(
+                "The armor makes you feel like you can stand up to anyone",
+                "Cursed Armor",
+                "Gain 1 Sanity." +
+                "Lose 1 Sanity if oyu lose control of the Cursed Armor." +
+                "Whenever you roll defence, you may add 1 to the result. If you do, lose 1 sanity." +
+                "This omen can't be stolen."
+        );
+
+        OmenCard deckOfManyThings = new OmenCard(
+                "What's the worst that could come of drawing a card?",
+                "Deck of Many Things",
+                "You may use this omen " +
+                "once during each of your to draw from the Deck of Many Things. Roll 4 dice and subtract 1 from the " +
+                "result for each small monster token on this card:" +
+                "8+ Moon:Gain 4 in a trait of your choice." +
+                "5-7 Star: Gain 1 in a trait of your choice." +
+                "3-4 Sun: Draw an item card." +
+                "1-2 Talons: Discard an item card or take 1 point of mental damage." +
+                "0 Void: Reduce one trait to its lowest value above the skull icon. If the haunt has begun, you die " +
+                "instead. " +
+                        "After rolling, place 1 small monster token on this card. If this card is dropped, traded, " +
+                        "or" +
+                        " stolen, any small monster tokens stay on the card."
+        );
+
+        OmenCard dimentionalShackles = new OmenCard(
+                "The shackles immediately latch onto your legs",
+                "Dimensional Shackles",
+                "Lose 1 Speed." +
+                        "Gain 1 Speed if you lose control of the Dimensional Shackles." +
+                        "Whenever you make an attack against an adventurer on your tile and win, you may give the " +
+                        "defender this card instead of inflicting damage." +
+                        "This omen can't be dropped or stolen, but it can be given (as above). It can also be traded " +
+                        "as long as both players agree.");
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
