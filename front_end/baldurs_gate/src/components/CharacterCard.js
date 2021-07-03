45d599ba9ef62579ffc782b7a845de92ebb7dcf4
klamb95 <klamb1995@gmail.com>
2021-07-03 13:12:50 +0100

    final commit before pushing

--- front_end/baldurs_gate/src/components/CharacterCard.js
+++ front_end/baldurs_gate/src/components/CharacterCard.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
 
 
+
 function CharacterCard({heroes}) {
 const HeroList = ({heroes}) => {
 
     const heroItems=heroes.map((hero, index) =>{
-        return <HeroItem hero = {hero} key = {index}/>
+        return <ListItem hero = {hero} key = {index}/>
     })
 
     const HeroDetail = ({selectedHero}) =>{
---
+++ front_end/baldurs_gate/src/components/ListItem.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+
+function ListItem({hero}){
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default ListItem;
